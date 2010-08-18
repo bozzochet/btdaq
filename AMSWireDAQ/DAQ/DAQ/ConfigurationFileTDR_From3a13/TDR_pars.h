ad33e438c773b16f0aef61892152f793f43fc883
duranti <18480084+bozzochet@users.noreply.github.com>
2010-08-18 15:33:57 +0000

    ConfigurationFileTDR stuffs for DSP after 3a13

---
+++ AMSWireDAQ/DAQ/DAQ/ConfigurationFileTDR_From3a13/TDR_pars.h
@@ -0,0 +1,61 @@
+#ifndef _TDRPARS_
+#define _TDRPARS_
+
+
+"Cluster Seed Factor S1 mult by 8"
+
+"Cluster lower threshold Factor S1 mult by 8"
+
+"Cluster Seed Factor S2 mult by 8"
+
+"Cluster lower threshold Factor S2 mult by 8"
+
+"Cluster Seed Factor K mult by 8"
+
+"Cluster lower threshold Factor K mult by 8"
+
+"SigmaRaw Factor for Channel exclusion in CN calc"
+
+"TAS Ladder type and column"
+
+"TAS Reduction Mode"
+
+"Reduction Mode S/N ratio mode"
+
+"Reduction mode: dynamic pedestals steps: 8 LSB step for small correction, 8 MSB step for large correction. (0=off)"
+
+"Reduction Mode: CN Output"
+
+"cTDR_AVRG_SEED (calibration step 3)"
+
+"cTDR_AVRG_THRE (calibration step 3)"
+
+"Threshold value for high occupancy channels (calib. step 4)"
+
+"Maximum cluster size"
+
+"Threshold for double trigger cal"
+
+"Number of events for  double trigger cal"
+
+"Flag for Get Calibration command"
+
+"Maximum cluster number per event, S-side"
+
+"Maximum cluster number per event, K-side"
+
+"number of event for calib pass 1"
+
+"number of event for calib pass 2"
+
+"number of event for calib pass 3"
+
+"number of event for calib pass 4"
+
+"Minimum channel number to have a reliable CN computation. Must be in the 2 ... 32 limit."
+
+"Flag mask for cluster seed rejection"
+
+"Single channel cluster exclusion threshold (0=off)"
+
+#endif
