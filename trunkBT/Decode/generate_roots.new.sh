21773bd4882729e597f336256e013097408191df
bozzochet <18480084+bozzochet@users.noreply.github.com>
2015-06-21 13:25:27 +0000

    improvements

--- trunkBT/Decode/generate_roots.new.sh
+++ trunkBT/Decode/generate_roots.new.sh
@@ -12,12 +12,16 @@ do
     
     ls -lrt Data_$SUFFIX/ | grep dat | awk '{ print $9 " " $5}' > FILELIST_$SUFFIX.txt
     
+    if [ ! -f FILELIST_$SUFFIX.old.txt ]; then
+        touch FILELIST_$SUFFIX.old.txt 
+    fi
+
     for i in `diff -y --suppress-common-lines FILELIST_$SUFFIX.txt FILELIST_$SUFFIX.old.txt | awk '{ print $1 }'`;
     do
 #	echo $i;
 #	echo ${i:0:10}
-	echo ./Decode ${i:0:10} 0 --rootdata /data/bt_dampe_jun2015/RootData_$SUFFIX/ --rawdata Data_$SUFFIX --caldata Calibrations_$SUFFIX/
-	./Decode ${i:0:10} 0 --rootdata /data/bt_dampe_jun2015/RootData_$SUFFIX/ --rawdata Data_$SUFFIX --caldata Calibrations_$SUFFIX/
+	echo ./Decode ${i:0:10} --rootdata /data/bt_dampe_jun2015/RootData_$SUFFIX/ --rawdata Data_$SUFFIX --caldata Calibrations_$SUFFIX/
+	./Decode ${i:0:10} --rootdata /data/bt_dampe_jun2015/RootData_$SUFFIX/ --rawdata Data_$SUFFIX --caldata Calibrations_$SUFFIX/
     done
 
 cp FILELIST_$SUFFIX.txt FILELIST_$SUFFIX.old.txt
