d53aaff3f0c0f39bba5d96977f1e78c88f155cca
bozzochet <18480084+bozzochet@users.noreply.github.com>
2017-11-01 16:25:13 +0000

    few scripts

--- trunkBT/DAQ/DAQ/generate_roots.sh
+++ trunkBT/DAQ/DAQ/generate_roots.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
 current_directory=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd $current_directory
-for i in `ls ./Calibrations/*02_ANC_*.cal`; do
-	real_name=${i:13}
+for i in `ls ./Calibrations/*.cal`; do
+	real_name=`basename $i`
+	#echo $real_name
 	numeric_value=${real_name:0:10}
-	current_string=${real_name%.*}
-	ancillary_value=${current_string:20}
-	real_root_names[0]="./Calibrations/summary-00_"$numeric_value"_ANC_"$ancillary_value".cal.pdf";
-	real_root_names[1]="./Calibrations/summary-01_"$numeric_value"_ANC_"$ancillary_value".cal.pdf";
+	real_root_name="./Calibrations/summary-00_"$numeric_value".cal.pdf";
+	#echo $real_root_name
 	exists=0
-	for real_root_name in ${real_root_names[@]}; do
-		if [ -e $real_root_name ]; then
-			exists=1
-			break
-		fi
-	done
+	if [ -e $real_root_name ]; then
+	    exists=1
+	fi
 	if [ "$exists" -eq "0" ]; then
 		echo "running on $real_name ..."
-		./SUMMARY $numeric_value $ancillary_value &> /dev/null
+		./SUMMARY_MUTE $numeric_value &> /dev/null
 	fi
 done
