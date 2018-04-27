3e596f771518d254b6c322b07b41ba7712007ee5
eberti <eberti@c541a117-7883-0410-a8dc-918c378dc4d8>
2018-04-27 13:03:59 +0000

    eugenio: Fixed TrigClass to work both with true and fake trigger handling

---
+++ Software/Perugia/DAQ/DAQ/launch_acquisition_original.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+unix_time=$(date +%s)
+echo "Time = $unix_time"
+
+nfile=$1
+nevent=$2
+
+command="./TakeEPP 0 INIT"
+echo $command
+$command
+sleep 5
+
+for i in `seq 0 $nfile`; do
+	command="./TakeEPP 0 CALIB"
+	echo $command
+	$command
+	sleep 5
+
+	command="./TakeEPP 0 TAKEDATA $nevent"
+	echo $command
+	$command
+	sleep 5
+done
