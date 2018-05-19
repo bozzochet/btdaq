ae6843a38d6fd99ba873d0767c21434856b493e4
eberti <eberti@c541a117-7883-0410-a8dc-918c378dc4d8>
2018-05-19 10:03:13 +0000

    eugenio: Update of the the readme ReadmeTIC and of the script launch_TIC.sh

--- tags/May2018_TIC/Software/Perugia/DAQ/DAQ/launch_TIC.sh
+++ tags/May2018_TIC/Software/Perugia/DAQ/DAQ/launch_TIC.sh
@@ -7,11 +7,11 @@ datediff() {
 	echo $(( (d1 - d2) / 1 )) #sec
 }
 
-log_dir="vaffanculo"
-master_dir="renzi"
-slave_dir="merda"
+log_dir="log"
+master_dir="."
+#slave_dir="."
 master_port=0
-slave_port=1
+#slave_port=1
 acq_exe="TakeEPP"
 gen_log="launch_acq_log.txt"
 
@@ -23,10 +23,10 @@ if [ ! -d $master_dir ]; then
 	echo "Master directory $master_dir not existing!"
 	exit 1;
 fi
-if [ ! -d $slave_dir ]; then
-	echo "Slave directory $slave_dir not existing!"
-	exit 1;
-fi
+#if [ ! -d $slave_dir ]; then
+#	echo "Slave directory $slave_dir not existing!"
+#	exit 1;
+#fi
 
 if [ $# -ne 2 ]; then
 	usage
@@ -46,15 +46,15 @@ date >> $gen_log
 echo "Master INIT is completed: " >> $gen_log
 sleep 5
 
-date >> $gen_log
-echo "Starting Slave INIT: " >> $gen_log
-cd $slave_dir
-command="$acq_exe $slave_port INIT"
-echo $command
-$command
-date >> $gen_log
-echo "Slave INIT is completed: " >> $gen_log
-sleep 5
+#date >> $gen_log
+#echo "Starting Slave INIT: " >> $gen_log
+#cd $slave_dir
+#command="$acq_exe $slave_port INIT"
+#echo $command
+#$command
+#date >> $gen_log
+#echo "Slave INIT is completed: " >> $gen_log
+#sleep 5
 
 calibration_time=$(date -d "Thu Jan  1 01:00:00 CET 1970")
 
@@ -78,30 +78,30 @@ for i in `seq 0 $nfile`; do
        		echo "Master CALIB is completed: " >> $gen_log
 		sleep 30
 
-		date >> $gen_log
-	        echo "Starting Slave CALIB: " >> $gen_log
-		cd $slave_dir
-		command="$acq_exe $slave_port CALIB 1 0"
-		echo $command
-		$command
-       		date >> $gen_log
-		echo "Master Slave is completed: " >> $gen_log
-		sleep 30
+#		date >> $gen_log
+#	        echo "Starting Slave CALIB: " >> $gen_log
+#		cd $slave_dir
+#		command="$acq_exe $slave_port CALIB 1 0"
+#		echo $command
+#		$command
+#      		date >> $gen_log
+#		echo "Master Slave is completed: " >> $gen_log
+#		sleep 30
 		
 		calibration_time=$(date)
 	fi 
 
 	unixtime=$(date +%s)
 
-	date >> $gen_log
-        echo "Starting Slave TAKEDATA: " >> $gen_log
-	cd $slave_dir
-	command="$acq_exe $slave_port TAKEDATA 1000000 0 0 $unixtime"
-	echo $command
-	$command |& tee $log_dir/slave_$unixtime.log &
-        sleep 60
-    
-	PID=`ps aux | grep $acq_exe | grep -v grep | awk '{ print $2 }'`
+#	date >> $gen_log
+#       echo "Starting Slave TAKEDATA: " >> $gen_log
+#	cd $slave_dir
+#	command="$acq_exe $slave_port TAKEDATA 1000000 0 0 $unixtime"
+#	echo $command
+#	$command |& tee $log_dir/slave_$unixtime.log &
+#       sleep 30
+#    
+#	PID=`ps aux | grep $acq_exe | grep -v grep | awk '{ print $2 }'`
 	
 	cd $master_dir
 	date >> $gen_log
@@ -113,11 +113,11 @@ for i in `seq 0 $nfile`; do
         
 	date >> $gen_log
         echo "Master TAKEDATA is completed: " >> $gen_log
-	sleep 60
+	sleep 30
 
-        kill $PID
- 
-	date >> $gen_log
-        echo "Slave TAKEDATA is completed: " >> $gen_log
-	#should we wait time here?
+#       kill $PID
+# 
+#	date >> $gen_log
+#       echo "Slave TAKEDATA is completed: " >> $gen_log
+#	#should we wait time here?
 done
