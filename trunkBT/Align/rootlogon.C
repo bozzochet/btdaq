ebfbb2eb75bc5617308643ac120c6260c17abdd8
Matteo Duranti <bozzo1983@gmail.com>
2022-12-15 12:47:52 +0100

    created branch with only old trunkFlight content

--- trunkBT/Align/rootlogon.C
+++
@@ -1,6 +0,0 @@
-{
-  const char *lname = "libEvent.so";
-  printf("Loading %s ... ", lname);
-  gSystem->Load("./libEvent.so");
-  printf("done\n");
-}
--- trunkBT/DAQ/DAQ/START.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd /home/testsys/amsdaq-code/trunkBT/DAQ/DAQ
-
-nohup ./launch_acquisition.sh > log/log_nohup.txt 2>&1 &
-
-sleep 5
-
-exit
